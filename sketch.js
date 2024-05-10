9184b28aa0791a6e9a43b7d74666ecb86eaa7c14
puffypenguin46 <161518195+puffypenguin46@users.noreply.github.com>
2024-05-10 11:30:21 -0600

    displayed an image
    display one of the test hallways

--- sketch.js
+++ sketch.js
@@ -8,56 +8,19 @@
 
 // Walker OOP Demo
 
-class Walker {
-  constructor(x, y, theColor) {
-    this.x = x;
-    this.y = y;
-    this.stepSize = 5;
-    this.color = theColor;
-    this.radius = 5;
-  }
-
-  display() {
-    fill(this.color);
-    circle(this.x, this.y, this.radius * 2);
-  }
-
-  move() {
-    let choice = random(100);
-    if (choice < 25) {
-      //move up
-      this.y -= this.stepSize;
-    }
-    else if (choice < 50) {
-      //move down
-      this.y += this.stepSize;
-    }
-    else if (choice < 75) {
-      //move right
-      this.x += this.stepSize;
-    }
-    else {
-      //move left
-      this.x -= this.stepSize;
-    }
-  }
+function preload() {
+  hallOne = loadImage('hallway 1.png');
+  hallTwo = loadImage('hall 2.png');
+  hallThree = loadImage('hall 3.png');
 }
 
-let theWalkers = [];
- 
 function setup() {
-  
 }
 
 function draw() {
-  for (let someWalker of theWalkers) {
-    someWalker.move();
-    someWalker.display();
-  }
+  createCanvas(windowHeight,windowWidth);
+  background(50);
 
+  image(hallOne, 10, 10);
 }
-function mousePressed() {
-  let theColor = color(random(255), random(255), random(255));
-  let myWalker = new Walker(mouseX, mouseY, "red");
-  theWalkers.push(myWalker);
-}
+
