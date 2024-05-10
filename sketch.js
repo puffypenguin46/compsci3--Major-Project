6e25d804306639406394a208ea950a2af3d6ba9c
puffypenguin46 <161518195+puffypenguin46@users.noreply.github.com>
2024-05-10 11:04:52 -0600

    random walker test
    not part of the game, just typed the walker demo in the wrong spot...

--- sketch.js
+++ sketch.js
@@ -6,13 +6,58 @@
 // - describe what you did to take this project "above and beyond"
 
 
+// Walker OOP Demo
+
+class Walker {
+  constructor(x, y, theColor) {
+    this.x = x;
+    this.y = y;
+    this.stepSize = 5;
+    this.color = theColor;
+    this.radius = 5;
+  }
+
+  display() {
+    fill(this.color);
+    circle(this.x, this.y, this.radius * 2);
+  }
+
+  move() {
+    let choice = random(100);
+    if (choice < 25) {
+      //move up
+      this.y -= this.stepSize;
+    }
+    else if (choice < 50) {
+      //move down
+      this.y += this.stepSize;
+    }
+    else if (choice < 75) {
+      //move right
+      this.x += this.stepSize;
+    }
+    else {
+      //move left
+      this.x -= this.stepSize;
+    }
+  }
+}
+
+let theWalkers = [];
+ 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  
 }
 
 function draw() {
-  background(220);
+  for (let someWalker of theWalkers) {
+    someWalker.move();
+    someWalker.display();
+  }
 
-  circle(mouseX, mouseY, 300);
 }
-
+function mousePressed() {
+  let theColor = color(random(255), random(255), random(255));
+  let myWalker = new Walker(mouseX, mouseY, "red");
+  theWalkers.push(myWalker);
+}
