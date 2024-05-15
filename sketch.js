92a4b39e08817f9ede443963813b14bc151497cb
puffypenguin46 <puffypenguin46@gmail.com>
2024-05-15 14:08:11 -0600

    Notes
    Following a tutorial on how to raycast while taking notes along the way on what I do and don't understand.

--- sketch.js
+++ sketch.js
@@ -8,19 +8,108 @@
 
 // Walker OOP Demo
 
+//ignore
+// function preload() {
+//   hallOne = loadImage('hallway 1.png');
+//   hallTwo = loadImage('hall 2.png');
+//   hallThree = loadImage('hall 3.png');
+// }
+// ------------------------------------------
+
+
+// create the map layout using a 2D ARRAY
+// Capital X marks a wall, P marks the player's starting position
+//lower case e in an enemy 
+const GAME_MAP = [
+  "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXX",
+  "X                                            X",
+  "X                                            X",
+  "X             X                P             X",
+  "X             X                              X",
+  "X                       e                    X",
+  "X                                            X",
+  "X                          XXXX              X",
+  "X       e                                    X",
+  "X                                            X",
+  "X                                            X",
+  "X                                            X",
+  "X                                            X",
+  "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXX",
+];
+
+const GRID_SIZE = 150;
+const PERSONAL_SPACE = 50;
+const RUN_SPEED = -25;
+const WALK_SPEED = -10;
+const MOUSE_SENSITIVITY = 0.0001;
+const CAM_X = 100;
+const CAM_Y = -75;
+const CAM_Z = 50;
+
+let wallTexture;
+let walls = [];
+let player;
+let enemies = [];
+
 function preload() {
-  hallOne = loadImage('hallway 1.png');
-  hallTwo = loadImage('hall 2.png');
-  hallThree = loadImage('hall 3.png');
+  wallTexture = loadImage('hallway 1.png');
 }
 
+
 function setup() {
+  // set canvas to WEBGl so render things in 3d
+  createCanvas(920, 600, WEBGL);
+
+  //game layout
+  //looping through 
+  for (let z = 0; z < GAME_MAP, length;  z ++){
+    for (let x = 0; x < GAME_MAP[z].length; x++) {
+      let tile = GAME_MAP[z][x];
+    }
+  }
 }
 
 function draw() {
-  createCanvas(windowHeight,windowWidth);
-  background(50);
-
-  image(hallOne, 10, 10);
+  background("blue");
 }
 
+class Player {
+  constructor(x, z) {
+    this.x = x;
+    this.z = z;
+    // where the player is facing
+    this.direction = -1; 
+  }
+
+  updateCamera() {
+    // search up how this part works cause i have no idea tbh
+    //a bunch of math 
+    let camX = this.x + Math.sin(this.direction) * CAM_X;
+    let camZ = this.z + Math.cos(this.direction) * CAM_Z;
+    let lookX = this.x - Math.sin(this.direction);
+    let lookZ = this.z - Math.cos(this.direction);
+    camera(camX, CAM_Y, camZ, lookX, CAM_Y, lookZ, 0, 1, 0);
+  }
+}
+
+class Enemy {
+  constructor(x, z) {
+    // no y position since everything is on the ground
+    this.x = x;
+    this.z = z;
+    //radius 
+    this.r = 50;
+  }
+
+  display() {
+    //search up how this push and pop thing works 
+    push();
+    // make a seamless ball
+    noStroke();
+    //move origin so enemy spawn in the right postion
+    translate(this.x, -this.r, this.z);
+    fill("red");
+    sphere(this.r);
+    pop();
+  }
+}
